e5af7356808eb4ceb1805c89ad604219ed64c912
Ron Crawford <roncrawford1300@gmail.com>
2022-03-20 23:01:39 -0400

    Test functions

--- script.js
+++ script.js
@@ -18,11 +18,45 @@ generateBtn.addEventListener("click", writePassword);
 // Begin original code
 
 // Password characters
-var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowerLetters = "abcdefghijklmnopqrstuvwxyz"
-var numbers = "1234567890"
-var specialCharacters = "`~!@#$%^&*()-_=+,<.>/?\|"
+var characters = {
+  upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerLetters = "abcdefghijklmnopqrstuvwxyz",
+  numbers = "1234567890",
+  specialCharacters = "`~!@#$%^&*()-_=+,<.>/?\|"
+}
 
+function promptUpperLetters() {
+  characters.upperLetters = window.confirm("Would you like your password to contain UPPERCASE characters?");
+  return characters.upperLetters;
+}
+
+function promptlowerLetters() {
+  characters.lowerLetters = window.confirm("Would you like your password to contain LOWERCASE characters?");
+  return characters.lowerLetters;
+};
+
+function promptNumbers() {
+  characters.numbers = window.confirm("Would you like your password to contain NUMBERS?");
+  return characters.numbers;
+}
+
+var promptspecialCharacters = function () {
+  characters.specialCharacters = window.confirm("Would you like your password to contain SPECIAL characters?");
+  return characters.specialCharacters;
+};
+
+// Password length validation
+function validateLengthInput(password) {
+  if (password.length >= 8 && password.length <= 128) {
+    password.validLength = true;
+    lenErrorDiv.style.display = "none";
+  } else {
+    if (!passwordInpt.validLength) {
+      lenErrorDiv.style.display = "block";
+    }
+    password.validLength = false;
+  }
+}
 
 // Define generatePassword function from above
 function.generatePassword() {
